3c28ae646dc0add704687e1346d0a7cc2f030829
masakiyokoi <masaki.yokoi.2h@stu.hosei.ac.jp>
2018-11-04 16:55:49 +0900

    add

--- load_data.py
+++ load_data.py
@@ -3,13 +3,13 @@ from chainer.datasets import mnist,split_dataset_random
 
 def load_data():
     train_val,test = mnist.get_mnist(withlabel = True,ndim = 1)
-    print(train_val[0][0].shape,test.shape[0][0].shape)
+    #print(train_val[0][0].shape,test.shape[0][0].shape)
 
     train,valid = split_dataset_random(train_val,50000,seed = 0)
 
     batchsize = 128
     train_iter = iterators.SerialIterator(train,batchsize)
     valid_iter = iterators.SerialIterator(valid,batchsize,repeat=False,shuffle=False)
-    test_iter = iterators.SerialIterator(test,batchsize,repeat=Fale,shuffle=False)
+    test_iter = iterators.SerialIterator(test,batchsize,repeat=False,shuffle=False)
 
-    return train_iter,valid_iter,test_iter
+    return test,train_iter,valid_iter,test_iter
