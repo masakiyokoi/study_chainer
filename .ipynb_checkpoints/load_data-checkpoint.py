08585880ad0e99872b64d77896187b958a46b299
user <user@user-no-MacBook-Air.local>
2018-11-04 14:56:12 +0900

    add

---
+++ .ipynb_checkpoints/load_data-checkpoint.py
@@ -0,0 +1,14 @@
+from chainer.datasets import mnist,split_dataset_random,iterators
+
+def load_data():
+    train_val,test = mnist.get_mnist(withlabel = True,ndim = 1)
+    print(train_val[0][0].shape,test.shape[0][0].shape)
+
+    train,valid = split_dataset_random(train_val,50000,seed = 0)
+
+    batchsize = 128
+    train_iter = iterators.SerialIterator(train,batchsize)
+    valid_iter = iterators.SerialIterator(valid,batchsize,repeat=False,shuffle=False)
+    test_iter = iterators.SerialIterator(test,batchsize,repeat=Fale,shuffle=False)
+
+    return train_iter,valid_iter,test_iter
